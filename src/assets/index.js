9983481004510877cc8dc338552793a779e0606e
Samy <Rhalimsami8@gmail.com>
2025-08-18 16:58:42 +0100

    commit for adding some pics

--- src/assets/index.js
+++ src/assets/index.js
@@ -34,8 +34,26 @@ import bts from "./company/bts.png";
 import est from "./company/est.png";
 import ministre from "./company/ministre.png";
 import s from "./company/s.png";
-
-
+import login from "./company/login_vinci.png";
+import fruits from "./company/fruits.jpg";
+import consul from "./company/consul.png";
+import space1 from "./company/spacebnb1.png";import space2 from "./company/spacebnb2.png";import space3 from "./company/spacebnb3.png";import space4 from "./company/spacebnb4.png";import space5 from "./company/spacebnb5.png";import space6 from "./company/spacebnb6.png";import space from "./company/spacebnb1.png";import space7 from "./company/spacebnb7.png";import space8 from "./company/spacebnb8.png";import space9 from "./company/spacebnb9.png";
+import pizzer1 from "./company/pizzer1.png";
+import pizzer2 from "./company/pizzer2.png";
+import pizzer3 from "./company/pizzer3.png";
+import pizzer4 from "./company/pizzer4.png";
+import pizzer5 from "./company/pizzer5.png";
+import pizzer6 from "./company/pizzer6.png";
+import pizzer7 from "./company/pizzer7.png";
+import pizzer8 from "./company/pizzer8.png";
+import pizzer10 from "./company/pizzer10.png";
+import pizzer11 from "./company/pizzer11.png";
+import pizzer12 from "./company/pizzer12.png";
+import pizzer13 from "./company/pizzer13.png";
+import pizzer14 from "./company/pizzer14.png";
+import space10 from "./company/spacebnb10.png";
+import space11 from "./company/spacebnb11.png";
+import img from "./company/img.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
@@ -43,7 +61,33 @@ import tripguide from "./tripguide.png";
 export {
   logo,
     est,
+    img,
     ministre,
+    login,
+    space1,
+    space2,
+    space3,
+    space4,
+    space5,
+    space6,
+    space7,
+    space8,
+    space9,
+    space10,
+    space11,
+    pizzer1,
+    pizzer2,
+    pizzer3, 
+    pizzer4,
+    pizzer5,
+    pizzer6,
+    pizzer7,
+    pizzer8,
+    pizzer10,
+    pizzer11,
+    pizzer12,
+    pizzer13,
+    pizzer14,
   backend,
   creator,
   mobile,
@@ -79,4 +123,6 @@ export {
   carrent,
   jobit,
   tripguide,
+  fruits,
+  consul,
 };
