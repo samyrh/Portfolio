eda8d896fa99f55de802597e4e9a1b3e3cc02911
Samy <Rhalimsami8@gmail.com>
2025-08-26 12:52:38 +0100

    commit for adding some description in section hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,22 +14,25 @@ const Hero = () => {
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Sami Rhalim</span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm a Full Stack Software Engineer <br className='sm:block hidden' />
-            Graduate from EMSI (École Marocaine des Sciences de l'Ingénieur)
-          </p>
-        </div>
+          <div>
+              <h1 className={`${styles.heroHeadText} text-white`}>
+                  Hi, I'm <span className='text-[#915EFF]'>Sami Rhalim</span>
+              </h1>
+              <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                  I'm a Full Stack Software Engineer <br className='sm:block hidden'/>
+                  Graduate from EMSI (École Marocaine des Sciences de l'Ingénieur) <br className='sm:block hidden'/>
+                  (Yes, I’ve had more NullPointerExceptions than cups of coffee ☕)
+              </p>
+
+          </div>
       </div>
 
-      <ComputersCanvas />
+        <ComputersCanvas/>
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+            <a href='#about'>
+                <div
+                    className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
                 y: [0, 24, 0],
