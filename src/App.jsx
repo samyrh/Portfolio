0f9cd74e59836186257e4fa5c8efaef37ddbbc95
Samy <Rhalimsami8@gmail.com>
2025-08-26 12:48:55 +0100

    commit for updating app js to include stars animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
 import { BrowserRouter } from "react-router-dom";
-
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 import Education from "./components/EducationCard.jsx";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Education />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <div className='relative z-0'>
+      <BrowserRouter>
+        <div className='relative z-0 bg-primary'>
+
+          {/* Global Stars Background Layer */}
+          <div className="fixed inset-0 -z-10">
+            <StarsCanvas />
+          </div>
+
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+            <Navbar />
+            {/* anchor for Home */}
+            <section id="home" className="scroll-mt-28">
+              <Hero />
+            </section>
+          </div>
+
+          {/* the rest already get ids from SectionWrapper if you used it */}
+          <About />
+          <Experience />
+          <Education />
+          <Tech />
+          <Works />
+          <Feedbacks />
           <Contact />
-          <StarsCanvas />
         </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
   );
-}
+};
 
 export default App;
